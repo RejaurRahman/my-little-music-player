94acf68466c33742f49d02f980a3ad39b7c204ee
RejaurRahman <rejaur-rahman@hotmail.co.uk>
2022-10-28 11:46:50 +0100

    added songs array list so the data is dynamic based on loaded song

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,61 @@
+const songCover = document.querySelector('img');
+const songTitle = document.getElementById('playerTitle');
+const artistTitle = document.getElementById('playerArtist');
 const music = document.querySelector('audio');
 const prevButton = document.getElementById('prevControl');
 const nextButton = document.getElementById('nextControl');
 const playButton = document.getElementById('playControl');
+
+const songs = [
+  {
+    name: 'soft_speak-siarate',
+    songName: 'soft speak',
+    artist: 'siarate',
+    cover: 'tower-fishing'
+  },
+  {
+    name: 'on_a_clear_morning-sunfish_grove',
+    songName: 'on a clear morning',
+    artist: 'sunfish grove',
+    cover: 'winter-trees'
+  },
+  {
+    name: 'omnia-helmut_schenker',
+    songName: 'omnia',
+    artist: 'helmut schenker',
+    cover: 'flowers-poppies'
+  },
+  {
+    name: 'those_who_arrived-volcan_peaks',
+    songName: 'those who arrived',
+    artist: 'volcan peaks',
+    cover: 'ocean-view'
+  },
+  {
+    name: 'someone_like_me-velvet_moon',
+    songName: 'someone like me',
+    artist: 'velvet moon',
+    cover: 'lake-forest'
+  },
+  {
+    name: 'small_things_grow-view_points',
+    songName: 'small things grow',
+    artist: 'view points',
+    cover: 'sky-cosmos'
+  },
+  {
+    name: 'meadow-thoughts-cody-francis',
+    songName: 'meadow thoughts',
+    artist: 'cody francis',
+    cover: 'swamp-trees'
+  },
+  {
+    name: 'go-mode-instrumental-version-nbh',
+    songName: 'go mode',
+    artist: 'nbhd nick',
+    cover: 'phone-wallpaper'
+  }
+];
 
 let isPlaying = false;
 
@@ -20,3 +74,12 @@ function pauseSong() {
 }
 
 playButton.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
+
+function loadSong(song) {
+  songTitle.textContent = song.songName;
+  artistTitle.textContent = song.artist;
+  music.src = `assets/music/${song.name}.mp3`;
+  songCover.src = `assets/image/${song.cover}.jpg`;
+}
+
+loadSong(songs[0]);
